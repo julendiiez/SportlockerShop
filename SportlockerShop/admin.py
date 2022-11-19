f634c26d68a8a50436ea52f054bcf46958f63e66
unknown <asierolaizola@opendeusto.es>
2022-11-19 11:08:10 +0100

    añadir models

--- SportlockerShop/admin.py
+++ SportlockerShop/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Usuario, Ropa, Compra
 # Register your models here.
--- SportlockerShop/models.py
+++ SportlockerShop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=50)
+    contraseña=models.CharField(max_length=15)
+    email=models.EmailField()
+
+class Ropa(models.Model):
+    nombre=models.CharField(max_length=50)
+    precio=models.IntegerField()
+    descripcion=models.TextField()
+    cantidad=models.IntegerField()
+
+class Compra(models.Model):
+    usuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    ropa=models.ManyToManyField(Ropa)
+    fechaDeCompra=models.DateField()
